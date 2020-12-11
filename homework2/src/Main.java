8152d6055b95a51d680409e2622c78c9ed0bf7e5
Ksenyia-Kasperchuck <kasperchuck98@gmail.com>
2020-12-11 11:08:01 +0300

    change

--- homework2/src/Main.java
+++ homework2/src/Main.java
@@ -5,7 +5,7 @@ import book.genre.Adventure;
 public class Main {
 
     public static void main(String[] args) {
-        Author danielDefo = new Author("Даниель", "Дефо");
+        Author danielDefo = new Author("Daniel", "Defo");
         Book robinsonCrusoe = new Adventure("Robinson Crusoe", danielDefo);
         robinsonCrusoe.print();
     }
